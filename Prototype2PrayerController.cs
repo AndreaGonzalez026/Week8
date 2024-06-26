6998856dfa1676dcb752b134d618f375b04afd88
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-06-25 23:07:51 -0600

    Update and rename PrayerController.cs to Prototype2PrayerController.cs

--- Prototype2PrayerController.cs
+++ Prototype2PrayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrayerController : MonoBehaviour
+public class PrayerController: MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
