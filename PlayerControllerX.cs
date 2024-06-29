16dd3f900515d17bda0b24fb6bfef4931924b141
Andrea Alheli Gonzalez <11069744@uvu.edu>
2024-06-29 00:54:28 -0600

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeWait = 1;
+    private float timeNextShoot = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > timeNextShoot)
+        {
+            timeNextShoot = Time.time + timeWait;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
